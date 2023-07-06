6dfb851c95410187530a23b64b1a3fbd6917cfcf
Valdeva Crema <valdeva_crema@harvard.edu>
2023-07-06 16:11:38 -0400

    feature flag implementation (#7)

--- cron.js
+++ cron.js
@@ -12,6 +12,12 @@ const queue_name = process.env.QUEUE_NAME;
 const etd_crontab = process.env.ETD_CRONTAB;
 const run_etd = parseInt(process.env.RUN_ETD);
 
+//Feature flags
+const dash_feature_flag = parseInt(process.env.DASH_FEATURE_FLAG);
+const alma_feature_flag = parseInt(process.env.ALMA_FEATURE_FLAG);
+const send_to_drs_feature_flag = parseInt(process.env.SEND_TO_DRS_FEATURE_FLAG);
+const drs_holding_record_feature_flag = parseInt(process.env.DRS_HOLDING_RECORD_FEATURE_FLAG);
+
 //ETD crontab
 if (run_etd) {
     consoleLogger.info("etd harvesting enabled"); 
@@ -22,7 +28,9 @@ if (run_etd) {
          // Send request to harvest etd
         const client = celery.createClient(amqp_url, amqp_url, queue_name);
         const task = client.createTask("tasks.tasks.do_task");
-        const result = task.applyAsync( [{'job_ticket_id': job_ticket_id, 'etd': true}] );
+        const result = task.applyAsync( [{'job_ticket_id': job_ticket_id, 'etd': true, 
+        	'dash_feature_flag': dash_feature_flag, 'alma_feature_flag': alma_feature_flag,
+        	'send_to_drs_feature_flag': send_to_drs_feature_flag, 'drs_holding_record_feature_flag': drs_holding_record_feature_flag}] );
         result.get().then(data => { client.disconnect(); });
     });
 }
